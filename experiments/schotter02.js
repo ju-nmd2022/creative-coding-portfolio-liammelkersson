bc1f952c38accdbf71484220246cbf6b60fffc2a
liammelkersson <liammelkersson@live.se>
2024-08-30 19:31:39 +0200

    schotter variants

---
+++ experiments/schotter02.js
@@ -0,0 +1,53 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(10);
+}
+
+function draw() {
+  // background(220, 210, 200);
+  angleMode(DEGREES);
+  strokeWeight(2);
+
+  // grid 17 x 22
+  let cols = 12;
+  let rows = 20;
+
+  let size = 25;
+
+  push();
+  //starting point
+  translate(125, 60);
+
+  noFill();
+  strokeWeight(1);
+  rectMode(CENTER);
+  // stroke(0, 70, 255);
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      let squareX = col * size;
+      let squareY = row * size;
+
+      let rotation = row * 2;
+      let offsetY = (row - squareY) * 0.02 * random(-1, 1);
+      let offsetX = (row - squareY) * 0.05 * random(-1, 1);
+
+      let r = map(squareX, 0, width, 50, 255);
+      let g = map(squareY, 0, width, 50, 255);
+      let b = map(squareX, 0, width, 255, 50);
+      let a = col * 25;
+
+      stroke(r, g, b, a);
+      // fill(random(0, 100),random(0, 10),random(0, 120), 20);
+      // fill(255, 255, 255);
+
+      push();
+      translate(squareX + offsetX, squareY + offsetY);
+      rotate(rotation * radians(45) * random(-1, 1));
+      rect(0, 0, size, size);
+      pop();
+    }
+  }
+  pop();
+  //   noLoop();
+}
---
+++ experiments/schotter05.js
@@ -0,0 +1,46 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  noFill();
+}
+
+function draw() {
+  background(20, 25);
+  angleMode(DEGREES);
+
+  let cols = 12;
+  let rows = 20;
+  let size = 25;
+
+  push();
+  translate(125, 60);
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      let squareX = col * size;
+      let squareY = row * size;
+
+      //https://p5js.org/reference/p5/frameCount/
+      let rotation = sin(frameCount * 2 + row * 10 + col * 10) * 45;
+      let offsetY = sin(frameCount * 3 + col * 10) * 10;
+      let offsetX = cos(frameCount * 3 + row * 10) * 10;
+
+      let r = random(0, 255);
+      let g = random(0, 255);
+      let b = 255;
+
+      stroke(r, g, b, 200);
+      strokeWeight(random(0.5, 3));
+
+      push();
+      translate(squareX + offsetX, squareY + offsetY);
+      rotate(rotation);
+
+      let newSize = size * random(0.5, 1.5);
+      rect(0, 0, newSize, newSize);
+
+      pop();
+    }
+  }
+  pop();
+}
